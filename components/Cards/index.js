277e123b47ca3dd6d7bb55678d756ac9ee290646
falmatad <42183720+falmatad@users.noreply.github.com>
2019-10-04 16:04:06 -0500

    article cards done, MVP complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -6,7 +6,36 @@
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
 //
-// <div class="card">
+//
+// Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then(response => {
+    const bootstrapArry = response.data.articles.bootstrap;
+    const javascriptArry = response.data.articles.javascript;
+    const jqueryArry = response.data.articles.jquery;
+    const nodeArry =response.data.articles.node;
+    const technologyArry= response.data.articles.technology;
+    
+    console.log(javascriptArry);
+    bootstrapArry.forEach(element => {
+        artclContainer.appendChild(createFeed(element.headline, element.authorPhoto, element.authorName));
+    });
+    javascriptArry.forEach(element => {
+        artclContainer.appendChild(createFeed(element.headline, element.authorPhoto, element.authorName));
+    });
+    jqueryArry.forEach(element => {
+        artclContainer.appendChild(createFeed(element.headline, element.authorPhoto, element.authorName));
+    });
+    nodeArry.forEach(element => {
+        artclContainer.appendChild(createFeed(element.headline, element.authorPhoto, element.authorName));
+    });
+    technologyArry.forEach(element => {
+        artclContainer.appendChild(createFeed(element.headline, element.authorPhoto, element.authorName));
+    });
+});
+
+
+/* <div class="card">
 //   <div class="headline">{Headline of article}</div>
 //   <div class="author">
 //     <div class="img-container">
@@ -14,86 +43,40 @@
 //     </div>
 //     <span>By {authors name}</span>
 //   </div>
-// </div>
-//
-// Create a card for each of the articles and add the card to the DOM.
+// </div> */
 
-axios.get('https://lambda-times-backend.herokuapp.com/articles').then(response => {
-    const bootstrapItem = response.articles.bootstrap;
-    const javascript = response.articles.javascript;
-    const jquery = response.articles.jquery;
-    const node = response.articles.node;
-    const technology = response.articles.technology;
-    
-    console.log(bootstrapItem);
-    
-    // bootstrap.forEach(element => {
-    //     articleFeed.appendChild(createFeed(element.headline, element.date, element.firstParagraph, element.secondParagraph, element.thirdParagraph))
-    //   })
-    
-    // javascript.forEach(element => {
-    //     articleFeed.appendChild(createFeed(element.title, element.date, element.firstParagraph, element.secondParagraph, element.thirdParagraph))
-    //     })
-
-    // jquery.forEach(element => {
-    //     articleFeed.appendChild(createFeed(element.title, element.date, element.firstParagraph, element.secondParagraph, element.thirdParagraph))
-    //     })
-
-    // node.forEach(element => {
-    //     articleFeed.appendChild(createFeed(element.title, element.date, element.firstParagraph, element.secondParagraph, element.thirdParagraph))
-    //     })
-
-    // technology.forEach(element => {
-    //     articleFeed.appendChild(createFeed(element.title, element.date, element.firstParagraph, element.secondParagraph, element.thirdParagraph))
-    //     })
-  });
-
-
-  const cardContainer = document.querySelector('.tabs');
+  const artclContainer = document.querySelector('.cards-container');
 // our re-usable component function
-function createFeed(title, date, firstParagraph, secondParagraph, thirdParagraph, )  {
+function createFeed(headline, authorPhoto, authorName)  {
     // define new elements
-    const article = document.createElement("div");
-    const artcTitle = document.createElement("h2");
-    const artcDate = document.createElement("p");
-    const p1 = document.createElement("p");
-    const p2 = document.createElement("p");
-    const p3 = document.createElement("p");
-    const expandBtn = document.createElement("span");
+    const artclCard = document.createElement("div");
+    const author = document.createElement("div");
+    const artclHeadline = document.createElement("div");
+    const imgContainer = document.createElement("div");
+    const img = document.createElement("img");
+    const byAuthorName = document.createElement("span");
     
     // append
     
-    article.appendChild(artcTitle);
-    article.appendChild(artcDate);
-    article.appendChild(p1);
-    article.appendChild(p2);
-    article.appendChild(p3);
-    article.appendChild(expandBtn);
+    artclCard.appendChild(artclHeadline);
+    artclCard.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(img);
+    author.appendChild(byAuthorName);
   
     
     // set class
     
-    article.classList.add("article");
-    artcTitle.classList.add("title");
-    artcDate.classList.add("date");
-    p1.classList.add("p1");
-    p2.classList.add("p2");
-    p3.classList.add("p3");
-    expandBtn.classList.add("expandButton");
+    artclCard.classList.add("card");
+    artclHeadline.classList.add("headline");
+    author.classList.add("author");
+    imgContainer.classList.add("img-container");
     
     
   //   add content
-    artcTitle.textContent = title;
-    artcDate.textContent = date;
-    p1.textContent = firstParagraph;
-    p2.textContent = secondParagraph;
-    p3.textContent = thirdParagraph;
-    expandBtn.textContent  = 'Expand';
+    artclHeadline.textContent = headline;
+    img.src = authorPhoto;
+    byAuthorName.textContent  = authorName;
 
-  // set up expand button
-    expandBtn.addEventListener("click", () => {
-        console.log('buttonClicked');
-        article.classList.toggle('article-open');
-      })
-    return article;
+    return artclCard;
   }
