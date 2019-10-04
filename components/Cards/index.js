386a868dddb88533df3e0e74f158f5cec0aebe4c
falmatad <42183720+falmatad@users.noreply.github.com>
2019-10-04 11:59:17 -0500

    falmata-dawano

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,37 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then(response => {
+    const tabsInfo = response.data.topics;
+
+    tabsInfo.forEach(topic => {
+        // console.log(Tabs(topic));
+        tabsContainer.appendChild(Tabs(topic));
+    })
+  });
+
+
+  const cardContainer = document.querySelector('.tabs');
+function Tabs(topics) {
+    // define new elements
+    const
+        topicsDiv = document.createElement('div'),
+        topicTitle = document.createElement('span')
+
+    // append
+    topicsDiv.appendChild(topicTitle);
+
+
+    // set class
+    topicsDiv.classList.add('tabs');
+    topicTitle.classList.add('tab');
+
+    //   add content
+    topicTitle.textContent = topics;
+
+    // check for extra features
+
+    // return
+    return topicsDiv;
+}
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,45 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics').then(response => {
+    const tabsInfo = response.data.topics;
+
+    tabsInfo.forEach(topic => {
+        // console.log(Tabs(topic));
+        tabsContainer.appendChild(Tabs(topic));
+    })
+  });
+
+/* <div class="tabs">
+<div class="topics">
+    <span class="title">TRENDING TOPICS:</span>
+</div>
+</div> */
+
+const tabsContainer = document.querySelector('.tabs');
+function Tabs(topics) {
+    // define new elements
+    const
+        topicsDiv = document.createElement('div'),
+        topicTitle = document.createElement('span')
+
+    // append
+    topicsDiv.appendChild(topicTitle);
+
+
+    // set class
+    topicsDiv.classList.add('tabs');
+    topicTitle.classList.add('tab');
+
+    //   add content
+    topicTitle.textContent = topics;
+
+    // check for extra features
+
+    // return
+    return topicsDiv;
+}
+
+  
